93308fecb63d3b6eeaed60c8f77d2b66544104a8
alvarodiaz12 <115473484+alvarodiaz12@users.noreply.github.com>
2025-04-10 08:55:43 +0200

    Funcionamiento de register y login con mysql perfecto, y homepane basico pero funcional con la api para realizar busqueda

--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -7,13 +7,15 @@ public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
-        try {
-            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+      try {
+          return new Configuration()
+                  .configure() 
+                  .buildSessionFactory();
+      } catch (Throwable ex) {
+          System.err.println("Initial SessionFactory creation failed." + ex);
+          throw new ExceptionInInitializerError(ex);
+      }
+  }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
